4b74268403d2d1906ceb02f18c99e44d84cd30f7
Sebastian Doerner <sebastian@sebastian-doerner.de>
2015-05-24 21:23:14 +0100

    Use common layout on the watch

--- wear/src/main/java/de/sebastian_doerner/temperatureconverter/ConverterActivity.java
+++ wear/src/main/java/de/sebastian_doerner/temperatureconverter/ConverterActivity.java
@@ -1,6 +1,5 @@
 package de.sebastian_doerner.temperatureconverter;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
@@ -9,17 +8,14 @@ import de.sebastian_doerner.common.BaseConverterActivity;
 
 public final class ConverterActivity extends BaseConverterActivity {
 
-    private TextView mTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_converter);
+        setContentView(R.layout.viewstub);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
     }
